fc65f4c0c7a8fa4c1f088e9f4ffec2c681be2a32
oguzhan coruk <coruk_61@hotmail.com>
2022-06-27 17:26:29 +0300

    ejs kuruldu navigation yapıldı

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const ejs = require('ejs')
 
 const app = express();
+//TEMPLATE ENGINE
+app.set("view engine", "ejs")
+//MIDDLEWARES
+app.use(express.static('public'))
+//ROUTES
+app.get('/index', (req, res) => {
 
-app.get('/', (req, res) => {
 
-    const blog = {
-        id: 1, title: "Blog title", description: "Blog description"
-    }
-    res.send(blog)
+    res.render("index")
+
+})
+app.get('/about', (req, res) => {
+
+
+    res.render("about")
+
+})
+app.get('/add_post', (req, res) => {
+
+
+    res.render("add_post")
+
 })
 
 const port = 3000;
